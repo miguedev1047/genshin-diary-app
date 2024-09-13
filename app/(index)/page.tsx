369b76577f40c33215e52dba96aa6ce01c707b85
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-13 15:49:26 -0500

    FEAT: home hero

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,12 +1,13 @@
 import { ParticlesBackground } from '@/components/shared/particles-background'
 import { Header } from './components/header'
+import { Hero } from './components/hero'
 
 export default function Home() {
   return (
     <>
       <Header />
       <main>
-        
+        <Hero />
       </main>
 
       <ParticlesBackground />
