ae292bee885183251df11fdacc5721e9622b8af4
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-14 04:18:10 -0500

    FEAT: top loader

--- app/(panel)/panel/layout.tsx
+++ app/(panel)/panel/layout.tsx
@@ -1,9 +1,18 @@
-import { PanelLayout } from "@/shared/layouts/panel/panel-layout"
+import { PanelLayout } from '@/shared/layouts/panel/panel-layout'
+import NextTopLoader from 'nextjs-toploader'
 
 export default function AdminPanelLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <PanelLayout>{children}</PanelLayout>
+  return (
+    <PanelLayout>
+      <NextTopLoader
+        color='#FABC3F'
+        showSpinner={false}
+      />
+      {children}
+    </PanelLayout>
+  )
 }
