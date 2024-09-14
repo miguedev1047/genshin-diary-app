2c432107044a607cf08efc1cdb097a06a427b854
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-14 01:11:16 -0500

    FEAT: setup login page

---
+++ app/(auth)/login/_components/login-form/login-form.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import {
+  Form,
+  FormLabel,
+  FormControl,
+  FormField,
+  FormItem,
+} from '@/components/ui/form'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useTransition } from 'react'
+import { InputBlock } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { LoginSchema } from '@/schemas'
+import { login } from '@/app/(auth)/login/_services/login'
+import { toast } from 'sonner'
+
+export function LoginForm() {
+  const [isPending, startTranstion] = useTransition()
+
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+  })
+
+  const handleSubmit = form.handleSubmit((values) => {
+    startTranstion(async () => {
+      const { status, message } = await login(values)
+
+      if (status === 201) {
+        toast.success(message)
+        return
+      }
+
+      toast.error(message)
+    })
+  })
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={handleSubmit}
+        className='w-full max-w-[400px] grid gap-4'
+      >
+        <FormField
+          name='email'
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Correo</FormLabel>
+              <FormControl>
+                <InputBlock
+                  disabled={isPending}
+                  placeholder='Ingresa tu correo'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          name='password'
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contraseña</FormLabel>
+              <FormControl>
+                <InputBlock
+                  type='password'
+                  disabled={isPending}
+                  placeholder='*************'
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+
+        <Button
+          disabled={isPending}
+          type='submit'
+        >
+          {isPending ? 'Ingresando...' : 'Entrar al panel'}
+        </Button>
+      </form>
+    </Form>
+  )
+}
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,17 @@
+import { Back } from './_components/back'
+import { LoginForm } from './_components/login-form'
+
 export default function PageLogin() {
   return (
-    <main></main>
-  );
+    <main className='flex min-h-screen flex-col items-center justify-center space-y-5 relative'>
+      <Back />
+
+      <div className='text-center'>
+        <h2 className='text-xl font-extrabold'>Admin Panel</h2>
+        <p className='opacity-50'>Ingresa al panel con tus credenciales.</p>
+      </div>
+
+      <LoginForm />
+    </main>
+  )
 }
