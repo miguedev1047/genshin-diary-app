c95f6fd25211a7a5a0cc7d59ea1181ec2bb76808
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-13 20:42:00 -0500

    FEAT: add character filter

--- app/(index)/_sections/characters/characters.tsx
+++ app/(index)/_sections/characters/characters.tsx
@@ -1,16 +1,12 @@
-import { GRID_LIST } from '@/consts/classes'
-import { ITEM_FILTERS } from '@/consts/general'
-import { Card, CardTitle, CardContent, CardHeader } from '@/components/ui/card'
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
 import { FocalLight } from '@/shared/components/focal-light'
 import { BorderBeam } from '@/components/magicui/border-beam'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
-import { Separator } from '@/components/ui/separator'
 import { CharacterProps } from '@/app/(index)/_sections/characters/character.type'
 import { getBorderColorByRarity } from '@/features/utils/rarity-color'
 import { formattedUrl } from '@/features/utils/formatted-names'
-import { QueryToggle } from '@/shared/components/query-toggle'
-import { SearchBar } from '@/shared/components/search-bar'
 import { cn } from '@/lib/utils'
+import { CharacterFilter } from './_components/character-filter'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -21,96 +17,13 @@ export async function Characters() {
 
       <Card className='relative overflow-hidden'>
         <CardHeader className='space-y-4'>
-          <CharacterHeader />
+          <CharacterFilter />
         </CardHeader>
 
-        <CardContent>
-         
-        </CardContent>
+        <CardContent></CardContent>
         <BorderBeam />
       </Card>
     </section>
-  )
-}
-
-function CharacterHeader() {
-  return (
-    <>
-      <div className='flex items-center justify-between gap-4 flex-wrap'>
-        <CardTitle className='uppercase font-extrabold op'>
-          Personajes
-        </CardTitle>
-
-        <SearchBar
-          queryParam='name'
-          placeholder='Buscar personaje'
-          className='w-[350px]'
-        />
-      </div>
-
-      <Card className='p-4 flex justify-center items-center gap-6'>
-        <ul className='flex space-x-2'>
-          {ITEM_FILTERS.star_filters.map((filter) => (
-            <QueryToggle
-              key={filter.value}
-              queryKey='stars'
-              queryValue={filter.value}
-            >
-              <Image
-                src={filter.src}
-                alt={filter.label}
-                width={128}
-                height={128}
-              />
-            </QueryToggle>
-          ))}
-        </ul>
-
-        <Separator
-          className='h-8'
-          orientation='vertical'
-        />
-
-        <ul className='flex space-x-3'>
-          {ITEM_FILTERS.element_filters.map((filter) => (
-            <QueryToggle
-              key={filter.value}
-              queryKey='element'
-              queryValue={filter.value}
-            >
-              <Image
-                src={filter.src}
-                alt={filter.label}
-                width={128}
-                height={128}
-              />
-            </QueryToggle>
-          ))}
-        </ul>
-
-        <Separator
-          className='h-8'
-          orientation='vertical'
-        />
-
-        <ul className='flex space-x-3'>
-          {ITEM_FILTERS.weapon_filters.map((filter) => (
-            <QueryToggle
-              key={filter.value}
-              queryKey='weapon'
-              queryValue={filter.value}
-            >
-              <Image
-                src={filter.src}
-                alt={filter.label}
-                width={128}
-                height={128}
-              />
-            </QueryToggle>
-          ))}
-        </ul>
-      </Card>
-    </>
   )
 }
 
