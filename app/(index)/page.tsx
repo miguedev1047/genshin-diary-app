b47a7f704ab3a9c0eeea85729648a6a093f6e7a7
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-14 05:13:08 -0500

    FEAT: home suspense

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -2,10 +2,11 @@ import { ParticlesBackground } from '@/shared/components/particles-background'
 import { Header } from '@/shared/layouts/index/header'
 import { Hero } from '@/app/(index)/_sections/hero'
 import { Characters } from '@/app/(index)/_sections/characters'
+import { Suspense } from 'react'
 
 export default async function Home() {
   return (
-    <>
+    <Suspense>
       <Header />
       <main>
         <Hero />
@@ -13,6 +14,6 @@ export default async function Home() {
       </main>
 
       <ParticlesBackground />
-    </>
+    </Suspense>
   )
 }
