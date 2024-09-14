f242f08409cb2fc9ee53f14d94f40901a7862905
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-14 03:43:09 -0500

    FEAT: import charts

--- app/(panel)/panel/index/page.tsx
+++ app/(panel)/panel/index/page.tsx
@@ -1,9 +1,25 @@
+import {
+  fetchElementChart,
+  fetchResumenChart,
+} from '@/app/(panel)/panel/index/_services/fetch'
 import { ContentLayout } from '@/shared/layouts/panel/content-layout'
+import { ElementChart } from '@/app/(panel)/panel/index/charts/element-chart'
+import { ResumenChart } from '@/app/(panel)/panel/index/charts/resumen-chart'
 
-export default function IndexPanelPage() {
+export default async function IndexPanelPage() {
+  const RESUMEN_DATA = await fetchResumenChart()
+  const RESUMEN_ELEMENT = await fetchElementChart()
+
   return (
     <ContentLayout title='Panel'>
-      <div>Panel</div>
+      <section className='grid grid-cols-8 gap-4'>
+      
+
+        <ResumenChart data={RESUMEN_DATA} />
+        <ElementChart data={RESUMEN_ELEMENT} />
+
+
+      </section>
     </ContentLayout>
   )
 }
