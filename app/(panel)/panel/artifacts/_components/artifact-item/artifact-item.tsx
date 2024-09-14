a4e33be340c719713ea8bab2a78d53be9580e329
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-14 16:14:38 -0500

    FEAT: artifact item card

---
+++ app/(panel)/panel/artifacts/_components/artifact-item/artifact-item.tsx
@@ -0,0 +1,61 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+  } from '@/components/ui/tooltip'
+  import { Card } from '@/components/ui/card'
+  import { AspectRatio } from '@/components/ui/aspect-ratio'
+  import { ArtifactItemProps } from '@/app/(panel)/panel/artifacts/_components/artifact-item/artifact-item.type'
+  import { formattedUrl } from '@/features/utils/formatted-names'
+  import { getBorderColorByRarity } from '@/features/utils/rarity-color'
+  import { cn } from '@/lib/utils'
+  import Link from 'next/link'
+  import Image from 'next/image'
+  
+  export function ArtifactItem(props: ArtifactItemProps) {
+    const { name, rarity, image_url } = props
+  
+    const FORMATTED_NAME = formattedUrl(name)
+    const URL = `/editor/artifact/${FORMATTED_NAME}`
+  
+    const ARTIFACT_IMAGE = image_url
+    const STAR_COLOR = getBorderColorByRarity(rarity)
+  
+    return (
+      <>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link
+                href={URL}
+                className={cn(
+                  'group/item flex aspect-[1/1] overflow-hidden rounded-[1rem] border border-muted/30 bg-background transition relative',
+                  STAR_COLOR
+                )}
+              >
+                <Card className='size-full transition duration-200 ease-in-out'>
+                  {ARTIFACT_IMAGE && (
+                    <AspectRatio ratio={1 / 1} className='p-8'>
+                      <Image
+                        src={ARTIFACT_IMAGE!}
+                        alt={name}
+                        width={720}
+                        height={1080}
+                        priority
+                        className='dark:grayscale object-cover w-full h-full transition-all duration-300 ease-in-out [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,.1))] group-hover/item:[mask-image:linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,1))] group-hover/item:scale-110 group-hover/item:grayscale-0'
+                      />
+                    </AspectRatio>
+                  )}
+                </Card>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side='bottom'>
+              <p>{name}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </>
+    )
+  }
+  
