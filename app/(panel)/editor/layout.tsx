44136e6a0bbd8694886e7d72778bd32c2f495714
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-15 17:29:49 -0500

    CHORE: update layout wrapper

--- app/(panel)/editor/layout.tsx
+++ app/(panel)/editor/layout.tsx
@@ -1,8 +1,18 @@
-export default function PanelLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return children
-  }
-  
+import { PanelLayout } from '@/shared/layouts/panel/panel-layout'
+import NextTopLoader from 'nextjs-toploader'
+
+export default function EditorLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <PanelLayout>
+      <NextTopLoader
+        color='#FABC3F'
+        showSpinner={false}
+      />
+      {children}
+    </PanelLayout>
+  )
+}
