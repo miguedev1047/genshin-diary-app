3848e868b13d24711b392feb92794f0c3ff3f29f
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-16 06:39:30 -0500

    FEAT: remove reacr query provider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,27 +1,24 @@
 'use client'
 
 import * as React from 'react'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SessionProvider } from 'next-auth/react'
 
-const queryClient = new QueryClient()
+// const queryClient = new QueryClient()
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextThemesProvider
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-        {...props}
-      >
-        <SessionProvider>{children}</SessionProvider>
-      </NextThemesProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <NextThemesProvider
+      attribute='class'
+      defaultTheme='system'
+      enableSystem
+      disableTransitionOnChange
+      {...props}
+    >
+      <SessionProvider>{children}</SessionProvider>
+    </NextThemesProvider>
   )
 }
