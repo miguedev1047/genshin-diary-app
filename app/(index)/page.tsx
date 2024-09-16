913ebde9bc1d011176c5945f75388849f1034fc6
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-16 06:39:47 -0500

    CHORE: remove character list

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,19 +1,18 @@
 import { ParticlesBackground } from '@/shared/components/particles-background'
 import { Header } from '@/shared/layouts/index/header'
 import { Hero } from '@/app/(index)/_sections/hero'
-import { Characters } from '@/app/(index)/_sections/characters'
-import { Suspense } from 'react'
+// import { Characters } from '@/app/(index)/_sections/characters'
+// import { Suspense } from 'react'
 
 export default async function Home() {
   return (
-    <Suspense>
+    <>
       <Header />
       <main>
         <Hero />
-        <Characters/>
       </main>
 
       <ParticlesBackground />
-    </Suspense>
+    </>
   )
 }
