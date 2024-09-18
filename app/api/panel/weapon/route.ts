cf90eab38c2de4395411c3ffa37e3c3cd783fe42
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-18 18:53:16 -0500

    FEAT: weapon endpoints

---
+++ app/api/panel/weapon/route.ts
@@ -0,0 +1,22 @@
+import { currentRole } from '@/data/auth'
+import { NextResponse } from 'next/server'
+import db from '@/lib/db'
+
+export async function GET(request: Request) {
+  const ROLE = await currentRole()
+
+  if (ROLE !== 'ADMIN') {
+    return NextResponse.json({ error: 'No autorizado' }, { status: 401 })
+  }
+
+  try {
+    const WEAPON = await db.weapons.findMany({
+      orderBy: [{ rarity: 'asc' }, { name: 'asc' }, { date_created: 'desc' }],
+      include: { ascensions: true, bests_characters: true },
+    })
+
+    return NextResponse.json(WEAPON, { status: 201 })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 404 })
+  }
+}
