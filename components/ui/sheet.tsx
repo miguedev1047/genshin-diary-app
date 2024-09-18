a73d85bca21256c66145ce32f08cef2fb254b579
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-18 18:57:43 -0500

    CHORE: minor changes

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -33,7 +33,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  'fixed z-50 gap-4 p-6 pr-0 bg-background shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+  'fixed z-50 gap-4 p-6 bg-background shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
   {
     variants: {
       side: {
