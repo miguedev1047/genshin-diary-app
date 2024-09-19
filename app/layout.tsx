5b38204079e3e5340898a6266880d9698e0ef6de
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-19 14:35:02 -0500

    FEAT: add site info

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,13 +4,13 @@ import { Inter } from 'next/font/google'
 import { Providers } from '@/app/providers'
 import { Toaster } from '@/components/ui/sonner'
 import { cn } from '@/lib/utils'
+import { PAGE_NAME } from '@/consts/site'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Genshin Diary',
-  description:
-    'Bienvenido a Genshin Diary una web que ofrece guias de genshin impact de una manera amigable.',
+  title: PAGE_NAME,
+  description: `Bienvenido a ${PAGE_NAME} una web que ofrece guias de genshin impact de una manera amigable.`,
 }
 
 export default function RootLayout({
--- shared/layouts/panel/panel-footer/panel-footer.tsx
+++ shared/layouts/panel/panel-footer/panel-footer.tsx
@@ -1,3 +1,5 @@
+import { PAGE_NAME } from '@/consts/site'
+
 export function PanelFooter() {
   const currentYear = new Date().getFullYear()
 
@@ -5,10 +7,9 @@ export function PanelFooter() {
     <div className='z-20 border-t border-border w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60'>
       <div className='mx-4 md:mx-8 flex h-14 items-center'>
         <p className='text-xs md:text-sm leading-loose text-muted-foreground text-left'>
-          Admin Panel - Hu Tao Mains {currentYear}
+          Admin Panel - {PAGE_NAME} {currentYear}
         </p>
       </div>
     </div>
   )
 }
-  
