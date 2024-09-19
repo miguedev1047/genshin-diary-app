b088ac297388dc17f9e181b65423223bbfa62c90
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-19 10:42:58 -0500

    FEAT: add material endpoints

---
+++ app/api/panel/material/route.ts
@@ -0,0 +1,20 @@
+import { currentRole } from '@/data/auth'
+import { NextResponse } from 'next/server'
+import db from '@/lib/db'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: Request) {
+  const ROLE = await currentRole()
+
+  if (ROLE !== 'ADMIN') {
+    return NextResponse.json({ error: 'No autorizado' }, { status: 401 })
+  }
+
+  try {
+    const MATERIALS = await db.materials.findMany()
+    return NextResponse.json(MATERIALS, { status: 201 })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 404 })
+  }
+}
