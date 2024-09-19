d8ee47b16bd745e8a4ba78852702ab412bdaf3b0
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-19 15:12:49 -0500

    CHORE: update map render

--- app/(panel)/panel/characters/_components/character-routes/character-routes.tsx
+++ app/(panel)/panel/characters/_components/character-routes/character-routes.tsx
@@ -6,14 +6,12 @@ import { CharacterRoutesProps } from '@/app/(panel)/panel/characters/_components
 export async function CharacterRoutes(props: CharacterRoutesProps) {
   const { params: PARAMS } = props
   const CHARACTERS = await getCharacters(PARAMS)
+  
+  const MAPPED_CHARACTERS = CHARACTERS?.map((item) => (
+    <li key={item.id}>
+      <CharacterItem {...item} />
+    </li>
+  ))
 
-  return (
-    <ul className={GRID_LIST}>
-      {CHARACTERS?.map((item) => (
-        <li key={item.id}>
-          <CharacterItem {...item} />
-        </li>
-      ))}
-    </ul>
-  )
+  return <ul className={GRID_LIST}>{MAPPED_CHARACTERS}</ul>
 }
