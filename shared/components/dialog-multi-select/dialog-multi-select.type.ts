b8763561af15f2127341a315fc95e1a099f463fd
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-21 21:51:56 -0500

    FEAT: add dialog multi select updated

---
+++ shared/components/dialog-multi-select/dialog-multi-select.type.ts
@@ -0,0 +1,90 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+
+/**
+ * Variants for the multi-select component to handle different styles.
+ * Uses class-variance-authority (cva) to define different styles based on "variant" prop.
+ */
+export const dialogMultiSelectVariants = cva(
+  'm-1 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300',
+  {
+    variants: {
+      variant: {
+        default:
+          'border-foreground/10 text-foreground bg-card hover:bg-card/80',
+        secondary:
+          'border-foreground/10 bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        destructive:
+          'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
+        inverted: 'inverted',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  }
+)
+
+/**
+ * Props for MenuSelect component
+ */
+export interface DialogMultiSelectProps
+  extends VariantProps<typeof dialogMultiSelectVariants> {
+  /**
+   * An array of option objects to be displayed in the multi-select component.
+   * Each option object has a label, value, and an optional icon.
+   */
+  items:
+    | {
+        /** The text to display for the option. */
+        name: string
+        /** The unique value associated with the option. */
+        id: string
+        /** Optional image URL for the option. */
+        image_url?: string | null | undefined
+      }[]
+    | undefined
+
+  /**
+   * Callback function triggered when the selected values change.
+   * Receives an array of the new selected values.
+   */
+  onValueChange: (value: string[]) => void
+
+  /** The default selected values when the component mounts. */
+  defaultValue: string[] | undefined
+
+  /** Disable selected values when the component mounts. */
+  disabledKeys?: string[] | undefined
+
+  /**
+   * Placeholder text to be displayed when no values are selected.
+   * Optional, defaults to "Select options".
+   */
+  placeholder?: string
+
+  /**
+   * Animation duration in seconds for the visual effects (e.g., bouncing badges).
+   * Optional, defaults to 0 (no animation).
+   */
+  animation?: number
+
+  /**
+   * Maximum number of items to display. Extra selected items will be summarized.
+   * Optional, defaults to 3.
+   */
+  maxCount?: number
+
+  /**
+   * If true, renders the multi-select component as a child of another component.
+   * Optional, defaults to false.
+   */
+  asChild?: boolean
+
+  /**
+   * Additional class names to apply custom styles to the multi-select component.
+   * Optional, can be used to add custom styles.
+   */
+  className?: string
+
+  isLoading?: boolean
+}
---
+++ shared/components/dialog-multi-select/index.ts
@@ -0,0 +1,1 @@
+export { DialogMultiSelect } from './dialog-multi-select'
