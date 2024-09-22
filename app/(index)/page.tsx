338fd3043050eac9582bd22cb3808f345e6cf234
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-22 03:28:19 -0500

    FEAT: add suspense

--- app/(index)/page.tsx
+++ app/(index)/page.tsx
@@ -1,18 +1,18 @@
-import { ParticlesBackground } from '@/shared/components/particles-background'
+// import { ParticlesBackground } from '@/shared/components/particles-background'
 import { Header } from '@/shared/layouts/index/header'
 import { Hero } from '@/app/(index)/_sections/hero'
 import { Characters } from '@/app/(index)/_sections/characters'
+import { Suspense } from 'react'
 
 export default async function Home() {
   return (
-    <>
+    <Suspense>
       <Header />
       <main className='py-20'>
         <Hero />
         <Characters />
       </main>
 
-      <ParticlesBackground />
-    </>
+    </Suspense>
   )
 }
