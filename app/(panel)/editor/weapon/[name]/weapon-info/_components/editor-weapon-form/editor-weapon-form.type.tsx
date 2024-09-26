13fca641c4ef8e32f9e40a86d001592451e29eb1
miguel angel <miguelangeljimenez1047@gmail.com>
2024-09-26 10:17:32 -0500

    CHORE: remove missing types file

--- app/(panel)/editor/weapon/[name]/weapon-info/_components/editor-weapon-form/editor-weapon-form.type.tsx
+++
@@ -1,7 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-export type EditorWeaponFormProps = {
-  data: Prisma.WeaponsGetPayload<{
-    include: { ascensions: true; bests_characters: true }
-  }> | null
-}
--- app/(panel)/editor/weapon/[name]/weapon-info/weapon-info.type.ts
+++
@@ -1,7 +0,0 @@
-import { Prisma } from '@prisma/client'
-
-export type WeaponInfoProps = {
-  data: Prisma.WeaponsGetPayload<{
-    include: { ascensions: true; bests_characters: true }
-  }> | null
-}
