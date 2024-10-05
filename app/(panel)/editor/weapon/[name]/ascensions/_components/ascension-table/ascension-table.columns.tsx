ab8522202056d5004068b27c35a17f4e7ac8c8d0
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-05 02:52:25 -0500

    FEAT: add ascension table

---
+++ app/(panel)/editor/weapon/[name]/ascensions/_components/ascension-table/ascension-table.columns.tsx
@@ -0,0 +1,99 @@
+'use client'
+
+import { MoraImg } from '@/assets/game'
+import { Prisma } from '@prisma/client'
+import { ColumnDef } from '@tanstack/react-table'
+import { DeleteButton } from '@/shared/layouts/panel/delete-button'
+import { Trash2 } from 'lucide-react'
+import { deleteAscension } from '@/editor/weapon/[name]/ascensions/_services/delete'
+import { MaterialItem } from '@/editor/weapon/[name]/ascensions/_components/material-item'
+import Image from 'next/image'
+
+type Ascension = Prisma.WeaponAscensionsGetPayload<{
+  include: { materials: true }
+}>
+
+export const ASCENSION_TABLE_COLUMNS: ColumnDef<Ascension>[] = [
+  {
+    accessorKey: 'ascension',
+    header: 'Ascensión',
+    cell: ({ row }) => {
+      const { ascension_level } = row.original
+      const [_, ASCENSION_NUMBER] = ascension_level.split('_')
+
+      return <p>Ascensión {ASCENSION_NUMBER}</p>
+    },
+  },
+  {
+    accessorKey: 'cost',
+    header: 'Costo',
+    cell: ({ row }) => {
+      const { cost } = row.original
+
+      const FORMATTED_COST = cost.toLocaleString('en-US', {
+        style: 'decimal',
+      })
+
+      return (
+        <article className='flex items-center gap-2'>
+          <figure className='relative size-20 aspect-auto bg-secondary p-1 rounded-md !overflow-hidden'>
+            <Image
+              priority
+              src={MoraImg.src}
+              alt='Mora Img'
+              width={720}
+              height={720}
+              className='object-contain size-full'
+            />
+            <div className='absolute inset-x-0 bottom-0 g-black/70b supports-[backdrop-filter]:bg-background/60 py-1 flex items-center justify-center z-20'>
+              <p>{FORMATTED_COST}</p>
+            </div>
+          </figure>
+          <p>Moras</p>
+        </article>
+      )
+    },
+  },
+  {
+    accessorKey: 'level',
+    header: 'Nivel',
+    cell: ({ row }) => {
+      const { level } = row.original
+      return <p>{level}</p>
+    },
+  },
+  {
+    accessorKey: 'materials',
+    header: 'Materiales',
+    cell: ({ row }) => {
+      const { materials } = row.original
+
+      return (
+        <ul className='flex items-center gap-2'>
+          {materials.map((material) => (
+            <li key={material.id}>
+              <MaterialItem {...material} />
+            </li>
+          ))}
+        </ul>
+      )
+    },
+  },
+
+  {
+    accessorKey: 'actions',
+    header: 'Acciones',
+    cell: ({ row }) => {
+      const { id } = row.original
+
+      return (
+        <DeleteButton
+          itemId={id}
+          onDelete={deleteAscension}
+        >
+          <Trash2 />
+        </DeleteButton>
+      )
+    },
+  },
+]
