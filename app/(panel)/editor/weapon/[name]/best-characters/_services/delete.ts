69c2b09d657521736f9b58e4a356c315a86b1c70
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-05 02:50:42 -0500

    CHORE: update status object

--- app/(panel)/editor/weapon/[name]/best-characters/_services/delete.ts
+++ app/(panel)/editor/weapon/[name]/best-characters/_services/delete.ts
@@ -4,7 +4,7 @@ import { currentRole } from '@/data/auth'
 import db from '@/lib/db'
 
 export async function deleteCharacter(id: string) {
-  if (!id) return { status: 403, message: 'Esta arma no existe.' }
+  if (!id) return { status: 403, message: 'Este personaje no existe.' }
 
   const ROLE = await currentRole()
   if (ROLE === 'USER') return { status: 403, message: 'No tienes permisos.' }
