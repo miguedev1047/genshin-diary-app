4acdc4a375806322acb1bd85398e01a187f12d65
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-05 02:54:35 -0500

    CHORE: remove props & add character hook

--- app/(panel)/editor/character/[name]/ascensions/ascensions.tsx
+++ app/(panel)/editor/character/[name]/ascensions/ascensions.tsx
@@ -1,16 +1,18 @@
-import { AscensionsProps } from '@/app/(panel)/editor/character/[name]/ascensions/ascensions.type'
+'use client'
+
 import { EditorCard } from '@/shared/layouts/panel/editor-card'
 import { AscensionTable } from '@/app/(panel)/editor/character/[name]/ascensions/_components/ascension-table'
 import { AscensionForm } from '@/app/(panel)/editor/character/[name]/ascensions/_components/ascension-form'
 import { columns } from '@/app/(panel)/editor/character/[name]/ascensions/_components/ascension-table/ascension-columns'
 import { Prisma } from '@prisma/client'
+import { useGetCharacter } from '@/editor/character/[name]/provider'
 
 export type Ascension = Prisma.AscensionCharacterGetPayload<{
   include: { materials: true }
 }>
 
-export function Ascension(props: AscensionsProps) {
-  const { data: CHARACTER } = props
+export function Ascension() {
+  const { data: CHARACTER } = useGetCharacter()
   const ASCENSIONS = CHARACTER?.ascensions as Array<Ascension>
 
   return (
