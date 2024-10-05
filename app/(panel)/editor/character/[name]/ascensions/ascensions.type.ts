884d2727ac4fe45d43264046dc248b5a55fb0269
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-05 02:50:11 -0500

    CHORE: remove type file

--- app/(panel)/editor/character/[name]/ascensions/ascensions.type.ts
+++
@@ -1,5 +0,0 @@
-import { CharacterProps } from '../_shared/types'
-
-export type AscensionsProps = {
-  data: CharacterProps | null
-}
--- app/(panel)/editor/character/[name]/character-info/character-info.type.ts
+++
@@ -1,5 +0,0 @@
-import { CharacterProps } from '../_shared/types'
-
-export type CharacterInfoProps = {
-  data: CharacterProps | null
-}
