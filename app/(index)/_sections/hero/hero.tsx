561e7affb462493160a6cdfee2562d1486804af0
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-05 22:40:30 -0500

    FEAT: add rainbow botton

--- app/(index)/_sections/hero/hero.tsx
+++ app/(index)/_sections/hero/hero.tsx
@@ -1,11 +1,11 @@
-import { Button } from '@/components/ui/button'
+import { RainbowButton } from '@/components/ui/rainbow-button'
 import AnimatedShinyText from '@/components/ui/animated-shiny-text'
 import Link from 'next/link'
 
 export function Hero() {
   return (
     <section className='relative mx-auto max-w-7xl h-[calc(100dvh-9rem)] px-6 text-center md:px-8 grid place-items-center'>
-      <div>
+      <div className='-translate-y-8'>
         <AnimatedShinyText className='inline-flex max-w-full'>
           <span className='animate-fade-in -translate-y-4 text-balance bg-gradient-to-br from-black from-30% to-black/40 bg-clip-text py-6 text-5xl font-medium opacity-0 [--animation-delay:200ms] leading-none tracking-tighter text-transparent sm:text-6xl md:text-7xl lg:text-8xl dark:from-white dark:to-white/40'>
             Tu web amigable para guías de Genshin Impact.
@@ -19,13 +19,12 @@ export function Hero() {
           Impact.
         </p>
 
-        <Button
-          variant='ringHover'
+        <Link
+          href='#characters'
           className='animate-fade-in -translate-y-4 opacity-0 [--animation-delay:600ms]'
-          asChild
         >
-          <Link href='#characters'>Ver personajes</Link>
-        </Button>
+          <RainbowButton>Ver personajes</RainbowButton>
+        </Link>
       </div>
     </section>
   )
