8ec94b36ee99e490fed66e16141848f301c881d2
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-06 21:48:34 -0500

    FEAT: add sortable list actions

---
+++ app/(panel)/editor/character/[name]/weapons/_services/delete.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { currentRole } from '@/data/auth'
+import db from '@/lib/db'
+
+export async function deleteWeapon(id: string) {
+  const ROLE = await currentRole()
+
+  if (ROLE === 'USER') {
+    return { status: 403, message: 'No tienes permisos.' }
+  }
+
+  try {
+    await db.weaponCharacter.delete({
+      where: {
+        id,
+      },
+    })
+
+    return { status: 201, message: 'Arma eliminada.' }
+  } catch (error) {
+    return { status: 500, message: 'Ocurrio un error.' }
+  }
+}
---
+++ app/(panel)/editor/character/[name]/weapons/_services/update.ts
@@ -0,0 +1,35 @@
+'use server'
+
+import db from '@/lib/db'
+import { currentRole } from '@/data/auth'
+import { WeaponCharacter } from '@prisma/client'
+
+export async function updateWeapons(data: Array<WeaponCharacter>) {
+  const ROLE = await currentRole()
+
+  if (ROLE === 'USER') {
+    return { status: 403, message: 'No tienes permisos.' }
+  }
+
+  const ITEMS = data.map((weapon, index) => ({
+    ...weapon,
+    order: index++ + 1,
+  }))
+
+  try {
+    ITEMS.forEach(async (weapon) => {
+      await db.weaponCharacter.update({
+        where: {
+          id: weapon.id,
+        },
+        data: {
+          order: weapon.order,
+        },
+      })
+    })
+
+    return { status: 201, message: 'Cambios guardados.' }
+  } catch (error) {
+    return { status: 500, message: 'Ocurrio un error.' }
+  }
+}
