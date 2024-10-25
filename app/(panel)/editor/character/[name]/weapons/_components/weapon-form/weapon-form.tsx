d391722a4413e9a7a487d80e56e0556a9fa8004e
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 21:45:58 -0500

    CHORE: add form sheet wrapper

--- app/(panel)/editor/character/[name]/weapons/_components/weapon-form/weapon-form.tsx
+++ app/(panel)/editor/character/[name]/weapons/_components/weapon-form/weapon-form.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
@@ -9,19 +8,7 @@ import {
   FormLabel,
   FormMessage,
 } from '@/components/ui/form'
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
 import { z } from 'zod'
-import { Plus } from 'lucide-react'
-import { ScrollArea } from '@radix-ui/react-scroll-area'
 import { useForm } from 'react-hook-form'
 import { WeaponSelector } from '@/editor/character/[name]/weapons/_components/weapon-selector'
 import { WeaponCharacterSchema } from '@/schemas'
@@ -30,6 +17,7 @@ import { useTransition } from 'react'
 import { createWeapons } from '@/editor/character/[name]/weapons/_services/create'
 import { useGetCharacter } from '@/editor/character/[name]/provider'
 import { useRouter } from 'next/navigation'
+import { FormSheet } from '@/shared/layouts/panel/form-sheet'
 import { toast } from 'sonner'
 
 const MAX_WEAPONS = 5
@@ -73,61 +61,32 @@ export function WeaponForm() {
   const handleReset = () => form.reset()
 
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button size='icon'>
-          <Plus />
-        </Button>
-      </SheetTrigger>
-      <SheetContent className='sm:max-w-[640px]'>
-        <SheetHeader>
-          <SheetTitle>Nueva arma</SheetTitle>
-          <SheetDescription>
-            Añade una nueva arma para el personaje
-          </SheetDescription>
-        </SheetHeader>
-
-        <ScrollArea className='w-full h-[800px]'>
-          <Form {...form}>
-            <form
-              onSubmit={handleSubmit}
-              className='grid gap-4 px-1 py-4'
-            >
-              <FormField
-                name='weapons'
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Armas</FormLabel>
-                    <FormControl>
-                      <WeaponSelector {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button
-                    variant='secondary'
-                    onClick={handleReset}
-                    type='button'
-                  >
-                    Cancelar
-                  </Button>
-                </SheetClose>
-                <Button
-                  disabled={isPending}
-                  type='submit'
-                >
-                  Agregar
-                </Button>
-              </SheetFooter>
-            </form>
-          </Form>
-        </ScrollArea>
-      </SheetContent>
-    </Sheet>
+    <FormSheet
+      title='Armas'
+      isLoading={isPending}
+      formId='weapon-form'
+    >
+      <Form {...form}>
+        <form
+          id='weapon-form'
+          onSubmit={handleSubmit}
+          className='grid gap-4'
+        >
+          <FormField
+            name='weapons'
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Armas</FormLabel>
+                <FormControl>
+                  <WeaponSelector {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </FormSheet>
   )
 }
