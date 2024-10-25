5da0949245582e5e2337936467b6beac8aca4d73
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 22:47:21 -0500

    FEAT: add passive button delete

---
+++ app/(panel)/editor/character/[name]/skills/passives/_components/passive-delete/passive-delete.tsx
@@ -0,0 +1,19 @@
+import { DeleteButton } from '@/shared/layouts/panel/delete-button'
+import { deletePassive } from '@/editor/character/[name]/skills/passives/_services/delete'
+import { PassiveDeleteProps } from '@/editor/character/[name]/skills/passives/_components/passive-delete/passive-delete.type'
+import { Trash2 } from 'lucide-react'
+
+export function PassiveDelete(props: PassiveDeleteProps) {
+  const { id } = props
+
+  return (
+    <div>
+      <DeleteButton
+        itemId={id}
+        onDelete={deletePassive}
+      >
+        <Trash2 />
+      </DeleteButton>
+    </div>
+  )
+}
