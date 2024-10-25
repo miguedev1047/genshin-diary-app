324d0c3f06a8986825a344a9cc76c0f0a4ab139a
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-25 00:49:17 -0500

    CHORE: add form sheet wrapper

--- app/(panel)/editor/weapon/[name]/best-characters/_components/best-characters-form/best-characters-form.tsx
+++ app/(panel)/editor/weapon/[name]/best-characters/_components/best-characters-form/best-characters-form.tsx
@@ -1,16 +1,6 @@
 'use client'
 
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-import {
   Form,
   FormControl,
   FormField,
@@ -19,11 +9,8 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { z } from 'zod'
-import { ScrollArea } from '@/components/ui/scroll-area'
 import { WeaponBestCharactersSchema } from '@/schemas'
-import { Button } from '@/components/ui/button'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Plus } from 'lucide-react'
 import { useTransition } from 'react'
 import { useForm } from 'react-hook-form'
 import { CharacterSelector } from '@/editor/weapon/[name]/best-characters/_components/character-selector'
@@ -31,6 +18,7 @@ import { useGetWeapon } from '@/editor/weapon/[name]/provider'
 import { createBestCharacters } from '@/editor/weapon/[name]/best-characters/_services/create'
 import { toast } from 'sonner'
 import { useRouter } from 'next/navigation'
+import { FormSheet } from '@/shared/layouts/panel/form-sheet'
 
 const MAX_CHARACTERS = 6
 
@@ -73,60 +61,32 @@ export function BestCharactersForm() {
   })
 
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button size='icon'>
-          <Plus />
-        </Button>
-      </SheetTrigger>
-      <SheetContent className='sm:max-w-[640px]'>
-        <SheetHeader>
-          <SheetTitle>Mejores personajes</SheetTitle>
-          <SheetDescription>
-            Selecciona los personajes que mejoren la habilidad de esta arma.
-          </SheetDescription>
-        </SheetHeader>
-
-        <ScrollArea className='w-full h-[800px]'>
-          <Form {...form}>
-            <form
-              onSubmit={handleSubmit}
-              className='grid gap-4 px-1 py-4'
-            >
-              <FormField
-                name='characters'
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Seleccionar personajes</FormLabel>
-                    <FormControl>
-                      <CharacterSelector {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button
-                    type='button'
-                    variant='secondary'
-                  >
-                    Cancelar
-                  </Button>
-                </SheetClose>
-                <Button
-                  type='submit'
-                  disabled={isPending}
-                >
-                  Guardar
-                </Button>
-              </SheetFooter>
-            </form>
-          </Form>
-        </ScrollArea>
-      </SheetContent>
-    </Sheet>
+    <FormSheet
+      title='Personajes'
+      isLoading={isPending}
+      formId='best-characters-form'
+    >
+      <Form {...form}>
+        <form
+          id='best-characters-form'
+          onSubmit={handleSubmit}
+          className='grid gap-4'
+        >
+          <FormField
+            name='characters'
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Seleccionar personajes</FormLabel>
+                <FormControl>
+                  <CharacterSelector {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </FormSheet>
   )
 }
