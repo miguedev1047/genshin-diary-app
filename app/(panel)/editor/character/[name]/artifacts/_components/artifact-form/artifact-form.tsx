9741be25546b7e0bf376fdc35f456165150a06b4
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 21:39:33 -0500

    CHORE: add form sheet wrapper

--- app/(panel)/editor/character/[name]/artifacts/_components/artifact-form/artifact-form.tsx
+++ app/(panel)/editor/character/[name]/artifacts/_components/artifact-form/artifact-form.tsx
@@ -1,16 +1,6 @@
 'use client'
 
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-import {
   Form,
   FormControl,
   FormField,
@@ -19,9 +9,6 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { z } from 'zod'
-import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Plus } from 'lucide-react'
 import { useForm } from 'react-hook-form'
 import { useTransition } from 'react'
 import { ArtifactCharacterSchema } from '@/schemas'
@@ -30,13 +17,14 @@ import { ArtifactSelector } from '@/editor/character/[name]/artifacts/_component
 import { createArtifacts } from '@/editor/character/[name]/artifacts/_services/create'
 import { useGetCharacter } from '@/editor/character/[name]/provider'
 import { useRouter } from 'next/navigation'
+import { FormSheet } from '@/shared/layouts/panel/form-sheet'
 import { toast } from 'sonner'
 
 const MAX_ARTIFACTS = 5
 
 export function ArtifactForm() {
   const { data: CHARACTER } = useGetCharacter()
-  
+
   const [isPending, startTransition] = useTransition()
   const { refresh } = useRouter()
 
@@ -71,64 +59,33 @@ export function ArtifactForm() {
     })
   })
 
-  const handleReset = () => form.reset()
-
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button size='icon'>
-          <Plus />
-        </Button>
-      </SheetTrigger>
-      <SheetContent className='sm:max-w-[640px]'>
-        <SheetHeader>
-          <SheetTitle>Nuevo artefacto</SheetTitle>
-          <SheetDescription>
-            Añade un nuevo artefacto para el personaje.
-          </SheetDescription>
-        </SheetHeader>
-
-        <ScrollArea className='w-full h-[800px]'>
-          <Form {...form}>
-            <form
-              onSubmit={handleSubmit}
-              className='grid gap-4 px-1 py-4'
-            >
-              <FormField
-                name='artifacts'
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Artefactos</FormLabel>
-                    <FormControl>
-                      <ArtifactSelector {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button
-                    variant='secondary'
-                    type='button'
-                    onClick={handleReset}
-                  >
-                    Cancelar
-                  </Button>
-                </SheetClose>
-                <Button
-                  disabled={isPending}
-                  type='submit'
-                >
-                  Guardar
-                </Button>
-              </SheetFooter>
-            </form>
-          </Form>
-        </ScrollArea>
-      </SheetContent>
-    </Sheet>
+    <FormSheet
+      title='Artefactos'
+      isLoading={isPending}
+      formId='artifact-form'
+    >
+      <Form {...form}>
+        <form
+          id='artifact-form'
+          onSubmit={handleSubmit}
+          className='grid gap-4'
+        >
+          <FormField
+            name='artifacts'
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Artefactos</FormLabel>
+                <FormControl>
+                  <ArtifactSelector {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </FormSheet>
   )
 }
