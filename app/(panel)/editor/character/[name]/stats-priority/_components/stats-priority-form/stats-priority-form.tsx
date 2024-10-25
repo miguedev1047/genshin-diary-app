d43064ae6d5aa4e8cd953797075949da3913d331
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 21:46:20 -0500

    CHORE: add form sheet wrapper

--- app/(panel)/editor/character/[name]/stats-priority/_components/stats-priority-form/stats-priority-form.tsx
+++ app/(panel)/editor/character/[name]/stats-priority/_components/stats-priority-form/stats-priority-form.tsx
@@ -1,16 +1,6 @@
 'use client'
 
 import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from '@/components/ui/sheet'
-import {
   Form,
   FormControl,
   FormField,
@@ -19,10 +9,6 @@ import {
   FormMessage,
 } from '@/components/ui/form'
 import { z } from 'zod'
-import { NONE } from '@/consts/general'
-import { Button } from '@/components/ui/button'
-import { ScrollArea } from '@/components/ui/scroll-area'
-import { Pencil, Plus } from 'lucide-react'
 import { useGetCharacter } from '@/editor/character/[name]/provider'
 import { useTransition } from 'react'
 import { useForm } from 'react-hook-form'
@@ -31,6 +17,7 @@ import { StatsPrioritySchema } from '@/schemas'
 import { createStats } from '@/editor/character/[name]/stats-priority/_services/create'
 import { updateStats } from '@/editor/character/[name]/stats-priority/_services/update'
 import { Input } from '@/components/ui/input'
+import { FormSheet } from '@/shared/layouts/panel/form-sheet'
 import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 
@@ -40,22 +27,22 @@ export function StatsPriorityForm() {
   const { refresh } = useRouter()
 
   const STATS = CHARACTER?.stats_priority
-  const STATS_EXISTS = STATS?.id !== undefined
+  const IS_EDITING = !!STATS?.id
 
   const form = useForm<z.infer<typeof StatsPrioritySchema>>({
     resolver: zodResolver(StatsPrioritySchema),
     defaultValues: {
-      circlet_stat: CHARACTER?.stats_priority?.circlet_stat ?? NONE,
-      globet_stat: CHARACTER?.stats_priority?.globet_stat ?? NONE,
-      order_priority: CHARACTER?.stats_priority?.order_priority ?? NONE,
-      sand_stat: CHARACTER?.stats_priority?.sand_stat ?? NONE,
-      character_id: CHARACTER?.id ?? NONE,
+      circlet_stat: STATS?.circlet_stat ?? '',
+      globet_stat: STATS?.globet_stat ?? '',
+      order_priority: STATS?.order_priority ?? '',
+      sand_stat: STATS?.sand_stat ?? '',
+      character_id: CHARACTER?.id ?? '',
     },
   })
 
   const handleSubmit = form.handleSubmit((values) => {
     startTransition(async () => {
-      if (STATS_EXISTS) {
+      if (IS_EDITING) {
         const { status, message } = await updateStats(values, STATS.id)
 
         if (status === 201) {
@@ -82,124 +69,92 @@ export function StatsPriorityForm() {
     })
   })
 
-  const handleReset = () => form.reset()
-
   return (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button size='icon'>{STATS_EXISTS ? <Pencil /> : <Plus />}</Button>
-      </SheetTrigger>
-      <SheetContent className='sm:max-w-[640px]'>
-        <SheetHeader>
-          <SheetTitle>
-            {STATS_EXISTS ? 'Editar' : 'Nueva'} estadistica
-          </SheetTitle>
-          <SheetDescription>
-            {STATS_EXISTS
-              ? 'Edita la estadistica seleccionada.'
-              : 'Añade una nueva estadistica para el personaje.'}
-          </SheetDescription>
-        </SheetHeader>
+    <FormSheet
+      title='Estadistica'
+      isEditing={IS_EDITING}
+      isLoading={isPending}
+      formId='stats-priority-form'
+    >
+      <Form {...form}>
+        <form
+          id='stats-priority-form'
+          onSubmit={handleSubmit}
+          className='grid gap-4'
+        >
+          <FormField
+            control={form.control}
+            name='sand_stat'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Reloj</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Vida Porcentual'
+                    {...field}
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <ScrollArea className='w-full h-[800px]'>
-          <Form {...form}>
-            <form
-              onSubmit={handleSubmit}
-              className='grid gap-4 px-1 py-4'
-            >
-              <FormField
-                control={form.control}
-                name='sand_stat'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Reloj</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder='Vida Porcentual'
-                        {...field}
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='globet_stat'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Caliz</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Bono de daño Pyro'
+                    {...field}
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='globet_stat'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Caliz</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder='Bono de daño Pyro'
-                        {...field}
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+          <FormField
+            control={form.control}
+            name='circlet_stat'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tiara</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Daño de Critico'
+                    {...field}
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-              <FormField
-                control={form.control}
-                name='circlet_stat'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Tiara</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder='Daño de Critico'
-                        {...field}
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name='order_priority'
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Prioridad</FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder='Prob. Crít. / Daño Crít. > Vida Porcentual > Maestria > Recarga de Energía'
-                        {...field}
-                        disabled={isPending}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <SheetFooter>
-                <SheetClose asChild>
-                  <Button
-                    variant='secondary'
-                    type='button'
-                    onClick={handleReset}
-                  >
-                    Cancelar
-                  </Button>
-                </SheetClose>
-                <Button
-                  disabled={isPending}
-                  type='submit'
-                >
-                  {STATS_EXISTS ? 'Guardar' : 'Agregar'}
-                </Button>
-              </SheetFooter>
-            </form>
-          </Form>
-        </ScrollArea>
-      </SheetContent>
-    </Sheet>
+          <FormField
+            control={form.control}
+            name='order_priority'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Prioridad</FormLabel>
+                <FormControl>
+                  <Input
+                    placeholder='Prob. Crít. / Daño Crít. > Vida Porcentual > Maestria > Recarga de Energía'
+                    {...field}
+                    disabled={isPending}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
+    </FormSheet>
   )
 }
