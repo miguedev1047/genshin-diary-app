4a8f935b06562878d47db3d98f4929615dd2ae49
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 21:40:13 -0500

    REFACTOR: artifact main

--- app/(panel)/editor/character/[name]/artifacts/artifacts.tsx
+++ app/(panel)/editor/character/[name]/artifacts/artifacts.tsx
@@ -1,36 +1,14 @@
-'use client'
-
 import { EditorCard } from '@/shared/layouts/panel/editor-card'
-import { useGetCharacter } from '@/editor/character/[name]/provider'
 import { ArtifactForm } from '@/editor/character/[name]/artifacts/_components/artifact-form'
-import { SortableList } from '@/shared/components/sortable-list'
-import { ArtifactItem } from '@/editor/character/[name]/artifacts/_components/artifact-item'
-import { updateArtifacts } from '@/editor/character/[name]/artifacts/_services/update'
-import { useDrag } from '@/features/hooks/use-drag'
+import { ArtifactList } from '@/editor/character/[name]/artifacts/_components/artifact-list/artifact-list'
 
 export function Artifacts() {
-  const { data: CHARACTER } = useGetCharacter()
-  const ARTIFACTS = CHARACTER?.artifacts ?? []
-
-  const { orderedItems, handleDragEnd } = useDrag({
-    items: ARTIFACTS,
-    updateFn: updateArtifacts,
-  })
-
   return (
     <EditorCard
       title='Mejores artefactos'
       renderForm={<ArtifactForm />}
     >
-      <SortableList
-        items={orderedItems}
-        onDragEnd={handleDragEnd}
-        renderItem={(item) => (
-          <SortableList.Item id={item.id}>
-            <ArtifactItem {...item} />
-          </SortableList.Item>
-        )}
-      />
+      <ArtifactList />
     </EditorCard>
   )
 }
