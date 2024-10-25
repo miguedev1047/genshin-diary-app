b5bbea84a1f8d6871af3eb95765126bc5cfc37ce
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-24 21:55:55 -0500

    FEAT: add talent actions

---
+++ app/(panel)/editor/character/[name]/skills/talents/_services/fetch.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { currentRole } from '@/data/auth'
+import db from '@/lib/db'
+
+export async function getTalent(id: string) {
+  const ROLE = await currentRole()
+
+  if (ROLE !== 'ADMIN') {
+    return null
+  }
+
+  try {
+    const TALENT = await db.talentsCharacter.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return TALENT
+  } catch (error) {
+    return null
+  }
+}
