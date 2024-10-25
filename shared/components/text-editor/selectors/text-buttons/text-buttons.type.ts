81e89e4fc466def2c6eb688a0bc1b4ababc141af
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-25 02:30:50 -0500

    CHORE: remove missing files

--- shared/components/text-editor/selectors/text-buttons/text-buttons.type.ts
+++
@@ -1,9 +0,0 @@
-import { useEditor } from '@tiptap/react'
-import { LucideIcon } from 'lucide-react'
-
-export type TextButtonsProps = {
-  name: string
-  icon: LucideIcon
-  command: (editor: ReturnType<typeof useEditor>['editor']) => void
-  isActive: (editor: ReturnType<typeof useEditor>['editor']) => boolean
-}
