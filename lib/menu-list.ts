7b2ada14849d3d25807a00d944b6583711abe816
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-27 08:33:55 -0500

    REFACTOR: sidebar items

--- lib/menu-list.ts
+++ lib/menu-list.ts
@@ -1,86 +1,26 @@
-import {
-  Bookmark,
-  LayoutGrid,
-  LucideIcon,
-  Group,
-  Users2,
-  Sword,
-  Crown,
-  Box,
-} from 'lucide-react'
+import { Users2, Sword, Box, Hourglass } from 'lucide-react'
 
-type Submenu = {
-  href: string
-  label: string
-  active: boolean
-}
-
-type Menu = {
-  href: string
-  label: string
-  active: boolean
-  icon: LucideIcon
-  submenus: Submenu[]
-}
-
-type Group = {
-  groupLabel: string
-  menus: Menu[]
-}
-
-export function getMenuList(pathname: string): Group[] {
-  return [
+export const SIDEBAR_DATA = {
+  sidebar_items: [
     {
-      groupLabel: '',
-      menus: [
-        {
-          href: '/panel',
-          label: 'Panel',
-          active: pathname === '/panel',
-          icon: LayoutGrid,
-          submenus: [],
-        },
-      ],
+      title: 'Personajes',
+      url: '/panel/characters',
+      icon: Users2,
     },
     {
-      groupLabel: 'Contenido',
-      menus: [
-        {
-          href: '/panel/characters',
-          label: 'Personajes',
-          active: pathname.includes('/characters'),
-          icon: Users2,
-          submenus: [],
-        },
-        {
-          href: '/panel/weapons',
-          label: 'Armas',
-          active: pathname.includes('/weapons'),
-          icon: Sword,
-          submenus: [],
-        },
-        {
-          href: '/panel/artifacts',
-          label: 'Artefactos',
-          active: pathname.includes('/artifacts'),
-          icon: Crown,
-          submenus: [],
-        },
-        {
-          href: '/panel/materials',
-          label: 'Materiales',
-          active: pathname.includes('/materials'),
-          icon: Box,
-          submenus: [],
-        },
-        // {
-        //   href: '/panel/teams',
-        //   label: 'Equipos',
-        //   active: pathname.includes('/teams'),
-        //   icon: Group,
-        //   submenus: [],
-        // },
-      ],
+      title: 'Armas',
+      url: '/panel/weapons',
+      icon: Sword,
     },
-  ]
+    {
+      title: 'Arterfactos',
+      url: '/panel/artifacts',
+      icon: Hourglass,
+    },
+    {
+      title: 'Materiales',
+      url: '/panel/materials',
+      icon: Box,
+    },
+  ],
 }
