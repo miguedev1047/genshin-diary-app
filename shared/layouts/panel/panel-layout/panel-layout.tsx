2b921cab08d5ba1df7891b68d0963af9a464e693
miguel angel <miguelangeljimenez1047@gmail.com>
2024-10-27 08:48:34 -0500

    CHORE: refactor panel layout

--- shared/layouts/panel/panel-layout/panel-layout.tsx
+++ shared/layouts/panel/panel-layout/panel-layout.tsx
@@ -1,45 +1,25 @@
 'use client'
 
-import { cn } from '@/lib/utils'
-import { useStore } from '@/features/store/use-store'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { PanelSidebar } from '../panel-sidebar'
+import { GridBackground } from '@/shared/components/grid-background'
 import { useSidebarToggle } from '@/features/store/use-sidebar-toggle'
-import { Sidebar } from '@/shared/layouts/panel/sidebar'
-import { PanelFooter } from '@/shared/layouts/panel/panel-footer'
-import GridPattern from '@/components/magicui/grid-pattern'
+import { useStore } from '@/features/store/use-store'
 
 export function PanelLayout({ children }: { children: React.ReactNode }) {
-  const sidebar = useStore(useSidebarToggle, (state) => state)
-
-  if (!sidebar) return null
+  const sidebarHook = useStore(useSidebarToggle, (state) => state)
+  if (!sidebarHook) return null
 
   return (
-    <>
-      <GridPattern
-        x={-1}
-        y={-1}
-        strokeDasharray={'4 4'}
-        className={cn(
-          '[mask-image:radial-gradient(1280px_circle_at_center,white,transparent)] fixed inset-0 z-0'
-        )}
-      />
+    <SidebarProvider
+      open={sidebarHook.isOpen}
+      onOpenChange={sidebarHook.setIsOpen}
+    >
+      <GridBackground />
 
-      <Sidebar />
-      <main
-        className={cn(
-          'min-h-[calc(100vh_-_60px)] bg-transparent transition-[margin-left] ease-in-out duration-300 sticky',
-          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
-        )}
-      >
-        {children}
-      </main>
-      <footer
-        className={cn(
-          'transition-[margin-left] ease-in-out duration-300',
-          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72'
-        )}
-      >
-        <PanelFooter />
-      </footer>
-    </>
+      <PanelSidebar />
+
+      {children}
+    </SidebarProvider>
   )
 }
