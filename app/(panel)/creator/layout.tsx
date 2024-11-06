5ba257e3496af03b7e51dcd5b48bc0a3a0a4a0a8
miguel angel <miguelangeljimenez1047@gmail.com>
2024-11-06 10:30:43 -0500

    CHORE: remove top loader

--- app/(panel)/creator/layout.tsx
+++ app/(panel)/creator/layout.tsx
@@ -1,18 +1,9 @@
 import { PanelLayout } from '@/shared/layouts/panel/panel-layout'
-import NextTopLoader from 'nextjs-toploader'
 
 export default function CreatorLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <PanelLayout>
-      <NextTopLoader
-        color='#FABC3F'
-        showSpinner={false}
-      />
-      {children}
-    </PanelLayout>
-  )
+  return <PanelLayout>{children}</PanelLayout>
 }
--- app/(panel)/panel/layout.tsx
+++ app/(panel)/panel/layout.tsx
@@ -1,18 +1,9 @@
 import { PanelLayout } from '@/shared/layouts/panel/panel-layout'
-import NextTopLoader from 'nextjs-toploader'
 
 export default function AdminPanelLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return (
-    <PanelLayout>
-      <NextTopLoader
-        color='#FABC3F'
-        showSpinner={false}
-      />
-      {children}
-    </PanelLayout>
-  )
+  return <PanelLayout>{children}</PanelLayout>
 }
