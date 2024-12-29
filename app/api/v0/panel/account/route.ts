13ce9f53974825084fce0ebf59427a9cd15ca9d4
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-29 05:45:31 -0500

    FEAT: add account query & end point ⚙️

---
+++ app/api/v0/panel/account/route.ts
@@ -0,0 +1,22 @@
+import { currentRole } from '@/data/auth'
+import { NextResponse } from 'next/server'
+import { db } from '@/lib/db'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: Request) {
+  const ROLE = await currentRole()
+
+  if (ROLE === 'USER') {
+    return NextResponse.json({ error: 'No autorizado' }, { status: 401 })
+  }
+
+  try {
+    const ACCOUNTS = await db.user.findMany({
+      orderBy: { name: 'asc' },
+    })
+    return NextResponse.json(ACCOUNTS, { status: 201 })
+  } catch (error) {
+    return NextResponse.json('Internal Server Error', { status: 404 })
+  }
+}
