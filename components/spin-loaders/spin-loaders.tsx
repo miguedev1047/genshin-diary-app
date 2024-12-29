1213fbe20634e565e31f6862edb4d884f6e208f6
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-29 05:47:03 -0500

    CHORE: add button spin loader component 🎨

--- components/spin-loaders/spin-loaders.tsx
+++ components/spin-loaders/spin-loaders.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { LoaderCircle } from 'lucide-react'
 import { Loader } from 'lucide-react'
 import { Card } from '@/components/ui/card'
 import { useTheme } from 'next-themes'
@@ -34,6 +36,22 @@ export function SpinLoaderInput() {
     <Card className='h-10 px-4 py-2'>
       <Loader className='animate-spin' />
     </Card>
+  )
+}
+
+export function SpinLoaderButton() {
+  return (
+    <Button
+      disabled
+      size='icon'
+    >
+      <LoaderCircle
+        className='animate-spin'
+        size={16}
+        strokeWidth={2}
+        aria-hidden='true'
+      />
+    </Button>
   )
 }
 
