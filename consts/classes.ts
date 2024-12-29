6c8b44154cfa5cdbeb68e2169d8f7ed42d0af065
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-28 20:17:10 -0500

    STYLE: add tier colors variable 🎨

--- consts/classes.ts
+++ consts/classes.ts
@@ -1,3 +1,11 @@
 export const GRID_LIST =
   'grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-6 gap-5'
 export const COL_SPAN = 'col-span-1 sm:col-span-2 md:col-span-4 xl:col-span-6'
+
+export const TIER_ROW_COLORS = {
+  S: 'bg-yellow-400',
+  A: 'bg-green-400',
+  B: 'bg-blue-400',
+  C: 'bg-purple-400',
+  D: 'bg-red-400',
+}
