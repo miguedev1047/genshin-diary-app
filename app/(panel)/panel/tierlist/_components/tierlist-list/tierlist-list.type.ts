659489a5c30d72eca226adb63f2517dbe27f9d91
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-28 20:10:12 -0500

    FEAT: add tierlist component 🎨

---
+++ app/(panel)/panel/tierlist/_components/tierlist-list/tierlist-list.type.ts
@@ -0,0 +1,9 @@
+import { Prisma } from '@prisma/client'
+
+export type TierlistListProps = {
+  data: Array<
+    Prisma.TierListGetPayload<{
+      include: { tiers: { include: { characters: true } } }
+    }>
+  >
+}
