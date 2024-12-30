e5fe5b3c2f93747b6cbbf2438c6c8402e5e424a6
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-29 19:58:49 -0500

    CHORE: remove react query developer tools ⚙️

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,6 @@ import * as React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { type ThemeProviderProps } from 'next-themes/dist/types'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { SessionProvider } from 'next-auth/react'
 
 const queryClient = new QueryClient()
@@ -21,7 +20,6 @@ export function Providers({ children, ...props }: ThemeProviderProps) {
       >
         <SessionProvider>{children}</SessionProvider>
       </NextThemesProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   )
 }
