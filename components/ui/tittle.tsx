4443facf3070a398e4fc6cd9f944cd12f2dee00b
miguel angel <miguelangeljimenez1047@gmail.com>
2024-12-31 17:42:15 -0500

    CHORE: minor changes ⚒️

--- components/ui/tittle.tsx
+++ components/ui/tittle.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
-const tittleVariants = cva('', {
+const tittleVariants = cva('text-balance', {
   variants: {
     size: {
       xs: 'text-xs font-light',
