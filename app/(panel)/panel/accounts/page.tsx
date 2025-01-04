09af986ee3f03dbaa00a18375d50a192e81664a0
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-04 01:12:54 -0500

    CHORE: remove section in account page ⚙️

--- app/(panel)/panel/accounts/page.tsx
+++ app/(panel)/panel/accounts/page.tsx
@@ -3,11 +3,11 @@ import { HeaderWrapper } from '@/components/header-wrapper'
 import { getAccounts } from '@/app/(panel)/panel/accounts/_services/fetch'
 import { Card } from '@/components/ui/card'
 import { columns } from '@/app/(panel)/panel/accounts/_components/accounts-table/accounts.column'
-import { AccountsTable } from '@/app/(panel)/panel/accounts/_components/accounts-table'
 import { AccuntsHeader } from '@/app/(panel)/_components/headers/accounts-header'
 import { AccountNoAccess } from '@/app/(panel)/panel/accounts/_components/account-no-access'
 import { currentRole } from '@/data/auth'
 import { User } from '@prisma/client'
+import { DataTable } from '@/components/data-tabla'
 
 export default async function AccountsPage() {
   const ACCOUNTS = (await getAccounts()) as Array<User>
@@ -21,14 +21,12 @@ export default async function AccountsPage() {
         <AccuntsHeader />
       </HeaderWrapper>
 
-      <section className='space-y-4'>
-        <Card className='p-6'>
-          <AccountsTable
-            data={ACCOUNTS ?? []}
-            columns={columns}
-          />
-        </Card>
-      </section>
+      <Card className='p-6'>
+        <DataTable
+          data={ACCOUNTS ?? []}
+          columns={columns}
+        />
+      </Card>
     </ContentLayout>
   )
 }
