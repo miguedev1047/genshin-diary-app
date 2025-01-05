5c1a3449703c3be8f0caaaad2d61d80030c172ba
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-05 13:14:28 -0500

    FEAT: add character actions ⚙️

---
+++ app/(index)/(routes)/characters/_services/fetch.ts
@@ -0,0 +1,40 @@
+import { db } from '@/lib/db'
+import { CharacterProps } from '../_types'
+
+type Props = {
+  name: string
+  element: string
+  weapon: string
+  stars: string
+}
+
+function filterCharacters(characters: Array<CharacterProps>, filters: Props) {
+  const { element, name, weapon, stars } = filters
+
+  return characters.filter((c) => {
+    const matches = [
+      element ? c.element.toLowerCase().includes(element.toLowerCase()) : true,
+      name ? c.name.toLowerCase().includes(name.toLowerCase()) : true,
+      weapon ? c.weapon.toLowerCase().includes(weapon.toLowerCase()) : true,
+      stars ? c.rarity.endsWith(stars) : true,
+    ]
+
+    return matches.every(Boolean)
+  })
+}
+
+export async function getCharacters(props: Props) {
+  try {
+    const CHARACTERS = await db.characters.findMany({
+      where: { is_public: true },
+      orderBy: [{ rarity: 'asc' }, { name: 'asc' }, { date_created: 'desc' }],
+      include: { images: true },
+    })
+
+    const FILTERED_CHARACTERS = filterCharacters(CHARACTERS, { ...props })
+
+    return FILTERED_CHARACTERS
+  } catch (error) {
+    return null
+  }
+}
