54c11ceb953677551a9fb84ab0548b7a6a8bf798
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-05 09:45:31 -0500

    CHORE: add conditional component 🎨

--- app/(index)/(dynamic-routes)/character/[id]/weapons/weapons.tsx
+++ app/(index)/(dynamic-routes)/character/[id]/weapons/weapons.tsx
@@ -1,14 +1,24 @@
 import { ViewCard } from '@/app/(index)/_components/view-card'
 import { WeaponsProps } from '@/app/(index)/(dynamic-routes)/character/[id]/weapons/weapons.type'
 import { WeaponItem } from '@/app/(index)/(dynamic-routes)/character/[id]/weapons/_components/weapon-item'
-import { cn } from '@/lib/utils'
+import { Title } from '@/components/ui/title'
 
 export function Weapons(props: WeaponsProps) {
   const { data } = props
   const WEAPONS = data.weapons ?? []
 
+  if (!WEAPONS.length) {
+    return (
+      <ViewCard title='Mejores armas'>
+        <Title className='text-center py-20 text-2xl opacity-70 font-extrabold uppercase'>
+          No hay elementos para mostrar
+        </Title>
+      </ViewCard>
+    )
+  }
+
   const MAPPED_WEAPONS = WEAPONS.map((weapon) => (
-    <li key={weapon.id} className={cn()}>
+    <li key={weapon.id}>
       <WeaponItem {...weapon} />
     </li>
   ))
