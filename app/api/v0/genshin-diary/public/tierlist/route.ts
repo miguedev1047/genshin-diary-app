0b09ec1a6e727bf41570d9a59525fbcc482cbe97
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-06 16:00:14 -0500

    FEAT: add tierlist endpoint ⚙️

---
+++ app/api/v0/genshin-diary/public/tierlist/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from 'next/server'
+import { db } from '@/lib/db'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(request: Request) {
+  try {
+    const TIERLIST = await db.tierList.findMany({
+      include: { tiers: { include: { characters: true } } },
+      orderBy: [{ order: 'asc' }, { date_created: 'desc' }],
+    })
+    
+    return NextResponse.json(TIERLIST, { status: 201 })
+  } catch (error) {
+    return NextResponse.json('Internal Server Error', { status: 404 })
+  }
+}
