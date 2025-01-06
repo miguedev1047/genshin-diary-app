f5df39fb0054d711e7021bd410d56e248ee582dd
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-06 14:54:36 -0500

    FEAT: add material header component 🎨

---
+++ components/headers/material-header/material-header.tsx
@@ -0,0 +1,75 @@
+import {
+  TooltipProvider,
+  TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+} from '@/components/ui/tooltip'
+import { Card, CardTitle } from '@/components/ui/card'
+import { MATERIAL_TYPES } from '@/consts/general'
+import { Button } from '@/components/ui/button'
+import { Plus } from 'lucide-react'
+import { QueryToggle } from '@/components/query-toggle'
+import { SearchBar } from '@/components/search-bar'
+import { HeaderProps } from '@/components/headers/_types'
+import { Title } from '@/components/ui/title'
+import Link from 'next/link'
+
+export function MaterialHeader(props: HeaderProps) {
+  const { isCreator = false } = props
+
+  return (
+    <>
+      <div className='flex items-center justify-between gap-4 flex-wrap'>
+        <Title
+          size='2xl'
+          className='font-extrabold uppercase'
+        >
+          Materiales
+        </Title>
+
+        <div className='flex items-center gap-2'>
+          <SearchBar
+            queryParam='name'
+            placeholder='Buscar material'
+            className='w-[350px]'
+          />
+
+          {isCreator && (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    size='icon'
+                    asChild
+                  >
+                    <Link href='/creator/material'>
+                      <Plus />
+                    </Link>
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent side='bottom'>
+                  <p>Crear material</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )}
+        </div>
+      </div>
+
+      <Card className='p-4 w-full'>
+        <ul className='grid grid-cols-4 gap-4'>
+          {MATERIAL_TYPES.map((filter) => (
+            <QueryToggle
+              key={filter.value}
+              queryKey='type'
+              queryValue={filter.value}
+              className='w-full'
+            >
+              {filter.label}
+            </QueryToggle>
+          ))}
+        </ul>
+      </Card>
+    </>
+  )
+}
