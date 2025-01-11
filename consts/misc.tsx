581e0a015795185296d4bef23406e399395fc1d7
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-11 00:07:09 -0500

    FEAT: setup v0.5.2 version ✅

--- consts/misc.tsx
+++ consts/misc.tsx
@@ -1,7 +1,7 @@
 import { HTMLReactParserOptions } from 'html-react-parser'
 
 export const PAGE_NAME = 'Genshin Diary'
-export const PAGE_VERSION = 'v0.5.0'
+export const PAGE_VERSION = 'v0.5.2'
 
 export const REACT_SCAN = false
 export const DEV_MODE = process.env.NODE_ENV === 'development'
@@ -16,7 +16,7 @@ export const PARSE_OPTIONS: HTMLReactParserOptions = {
   replace(domNode) {
     if (domNode.type === 'text') {
       const { data } = domNode
-      return <span className='!text-primary text-pretty'>{data}</span>
+      return <span className='text-pretty'>{data}</span>
     }
   },
 }
