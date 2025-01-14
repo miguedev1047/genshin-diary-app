06f6ee5a2d4d76dd5f70cc96dfc3533b364520b4
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-13 21:12:59 -0500

    CHORE: remove card component 🎨

--- components/headers/material-header/material-header.tsx
+++ components/headers/material-header/material-header.tsx
@@ -4,7 +4,6 @@ import {
   Tooltip,
   TooltipContent,
 } from '@/components/ui/tooltip'
-import { Card, CardTitle } from '@/components/ui/card'
 import { MATERIAL_TYPES } from '@/consts/general'
 import { Button } from '@/components/ui/button'
 import { Plus } from 'lucide-react'
@@ -55,21 +54,18 @@ export function MaterialHeader(props: HeaderProps) {
           )}
         </div>
       </div>
-
-      <Card className='p-4 w-full'>
-        <ul className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-          {MATERIAL_TYPES.map((filter) => (
-            <QueryToggle
-              key={filter.value}
-              queryKey='type'
-              queryValue={filter.value}
-              className='w-full'
-            >
-              {filter.label}
-            </QueryToggle>
-          ))}
-        </ul>
-      </Card>
+      <ul className='grid sm:grid-cols-2 lg:grid-cols-4 gap-4'>
+        {MATERIAL_TYPES.map((filter) => (
+          <QueryToggle
+            key={filter.value}
+            queryKey='type'
+            queryValue={filter.value}
+            className='w-full'
+          >
+            {filter.label}
+          </QueryToggle>
+        ))}
+      </ul>
     </>
   )
 }
