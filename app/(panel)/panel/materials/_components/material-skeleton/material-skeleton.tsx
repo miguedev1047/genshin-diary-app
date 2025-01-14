4e427df94ae2f42e792a4e89403de04b7034484e
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-13 21:15:27 -0500

    CHORE: update material skeleton component 🎨

--- app/(panel)/panel/materials/_components/material-skeleton/material-skeleton.tsx
+++ app/(panel)/panel/materials/_components/material-skeleton/material-skeleton.tsx
@@ -1,11 +1,10 @@
 import { Card } from '@/components/ui/card'
 import { Skeleton } from '@/components/ui/skeleton'
-import { GRID_LIST } from '@/consts/classes'
 
 export function MaterialSkeleton() {
   return (
-    <ul className={GRID_LIST}>
-      {Array.from({ length: 12 }).map((_, index) => (
+    <ul className='grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 gap-5'>
+      {Array.from({ length: 20 }).map((_, index) => (
         <Card
           key={index}
           className='aspect-[1/1]'
