7c70b15fa14e2fd2ae360f01e36465d29701eff1
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-15 00:42:11 -0500

    CHORE: refactor misc ⚙️

--- consts/misc.ts
+++ consts/misc.ts
@@ -1,7 +1,7 @@
 import { HTMLReactParserOptions } from 'html-react-parser'
 
 export const PAGE_NAME = 'Genshin Diary'
-export const PAGE_VERSION = 'v0.5.3'
+export const PAGE_VERSION = 'v0.5.4'
 
 export const REACT_SCAN = false
 export const DEV_MODE = process.env.NODE_ENV === 'development'
@@ -10,12 +10,3 @@ export const API_PREFIX = '/api/v0/genshin-diary'
 
 export const NONE = ''
 export const DEFAULT_IMAGE = 'https://placehold.co/600?text=Genshin+Diary'
-
-export const PARSE_OPTIONS: HTMLReactParserOptions = {
-  replace(domNode) {
-    if (domNode.type === 'text') {
-      const { data } = domNode
-      return <>{data}</>  
-    }
-  },
-}
