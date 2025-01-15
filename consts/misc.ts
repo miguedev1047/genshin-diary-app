7505da0845379d8d0ffabde99184d5e6240bfee6
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-15 17:06:19 -0500

    FEAT: setup v0.5.5 ✅

--- consts/misc.ts
+++ consts/misc.ts
@@ -1,7 +1,5 @@
-import { HTMLReactParserOptions } from 'html-react-parser'
-
 export const PAGE_NAME = 'Genshin Diary'
-export const PAGE_VERSION = 'v0.5.4'
+export const PAGE_VERSION = 'v0.5.5'
 
 export const REACT_SCAN = false
 export const DEV_MODE = process.env.NODE_ENV === 'development'
