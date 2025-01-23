e524a9db347fb53fa0688d851f3dad7c538f7160
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-23 06:26:01 -0500

    FEAT: setup v0.5.6 ✅

--- consts/misc.ts
+++ consts/misc.ts
@@ -1,5 +1,5 @@
 export const PAGE_NAME = 'Genshin Diary'
-export const PAGE_VERSION = 'v0.5.5'
+export const PAGE_VERSION = 'v0.5.6'
 
 export const REACT_SCAN = false
 export const DEV_MODE = process.env.NODE_ENV === 'development'
@@ -8,3 +8,5 @@ export const API_PREFIX = '/api/v0/genshin-diary'
 
 export const NONE = ''
 export const DEFAULT_IMAGE = 'https://placehold.co/600?text=Genshin+Diary'
+
+export const STALE_MS = 60 * 60 * 3600
