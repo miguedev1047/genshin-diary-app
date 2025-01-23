e64f6dfdb1f730a8e3ee440c9139bfcca15c8367
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-23 18:08:08 -0500

    CHORE: reorder maaterials endpoint ⚙️

--- app/api/v0/genshin-diary/material/route.ts
+++ app/api/v0/genshin-diary/material/route.ts
@@ -6,6 +6,7 @@ export const dynamic = 'force-dynamic'
 export async function GET(request: NextRequest) {
   try {
     const MATERIALS = await db.materials.findMany({
+      orderBy: [{ date_created: 'asc' }, { rarity: 'desc' }, { name: 'asc' }],
       where: {
         OR: [
           { type: 'MATERIAL_UPGRADE' },
