18f052317d1c77074eff0673bd54da15118341e0
miguel angel <miguelangeljimenez1047@gmail.com>
2025-01-23 06:33:11 -0500

    FEAT: add endpoint character full by id 🖥️

---
+++ app/api/v0/genshin-diary/character/full/id/[id]/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from 'next/server'
+import { db } from '@/lib/db'
+
+export const dynamic = 'force-dynamic'
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const CHARACTER_ID = params.id
+
+  try {
+    const CHARACTER = await db.characters.findUnique({
+      where: { id: CHARACTER_ID },
+      include: {
+        artifacts: {
+          orderBy: { order: 'asc' },
+          include: { artifact_set: { orderBy: { order: 'asc' } } },
+        },
+        ascensions: { orderBy: { level: 'asc' }, include: { materials: true } },
+        images: true,
+        stats_priority: true,
+        video_guide: true,
+        weapons: { orderBy: { order: 'asc' } },
+        teams: {
+          orderBy: { order: 'asc' },
+          include: { characters: { orderBy: { order: 'asc' } } },
+        },
+        talents_ascension: {
+          orderBy: { level: 'asc' },
+          include: { materials: { orderBy: [{ date_created: 'asc' }] } },
+        },
+        talents: { orderBy: [{ order: 'asc' }, { date_created: 'asc' }] },
+        passives: { orderBy: { order: 'asc' } },
+        constellations: { orderBy: { order: 'asc' } },
+        tierlist: { include: { tier_row: true } },
+      },
+    })
+
+    return NextResponse.json(CHARACTER, { status: 201 })
+  } catch (error) {
+    return new NextResponse('Internal Server Error', { status: 404 })
+  }
+}
