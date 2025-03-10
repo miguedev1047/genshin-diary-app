32d09c26d935f1345a8378e7381731ac3be242b6
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-10 18:58:08 -0500

    FEAT: add empty component 🎨

--- app/(panel)/panel/characters/_components/character-routes/character-routes.tsx
+++ app/(panel)/panel/characters/_components/character-routes/character-routes.tsx
@@ -2,11 +2,17 @@ import { GRID_LIST } from '@/consts/classes'
 import { getCharacters } from '@/app/(panel)/panel/characters/_services/fetch'
 import { CharacterItem } from '@/app/(panel)/panel/characters/_components/character-item'
 import { CharacterRoutesProps } from '@/app/(panel)/panel/characters/_components/character-routes/character-routes.type'
+import { EMPTY_LIST } from '@/consts/misc'
+import { EmptyList } from '@/components/empty-list'
 
 export async function CharacterRoutes(props: CharacterRoutesProps) {
   const { params: PARAMS } = props
   const CHARACTERS = await getCharacters(PARAMS)
-  
+
+  if (!CHARACTERS || CHARACTERS.length === EMPTY_LIST) {
+    return <EmptyList text='No hay personajes disponibles' />
+  }
+
   const MAPPED_CHARACTERS = CHARACTERS?.map((item) => (
     <li key={item.id}>
       <CharacterItem {...item} />
