41b8ac58f50ab5784ff999151840ba3f9cb09cea
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-14 16:44:24 -0500

    CHORE: refactor logo component ⚙️

--- app/(index)/_components/logo/logo.tsx
+++ app/(index)/_components/logo/logo.tsx
@@ -1,19 +1,39 @@
+'use client'
+
 import { Badge } from '@/components/ui/badge'
-import GenshinDiaryLogo from '@/public/genshin-diary-logo.png'
+import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import Link from 'next/link'
+import GenshinDarkLogo from '@/public/dark-logo.svg'
+import GenshinLightLogo from '@/public/light-logo.svg'
+import { useEffect, useState } from 'react'
+
+const DarkLogo = GenshinDarkLogo.src
+const LightLogo = GenshinLightLogo.src
+
 export function Logo() {
+  const { theme } = useTheme()
+  const [currentTheme, setTheme] = useState(theme)
+  
+  const CURRENT_LOGO = theme !== 'dark' ? DarkLogo : LightLogo
+
+  useEffect(() => {
+    setTheme(theme)
+  }, [theme])
+
+  console.log(currentTheme)
 
   return (
     <Link
       href={'/'}
-      className='w-[128px] h-auto flex items-center gap-3'
+      className='w-[136px] h-auto flex items-center gap-3'
     >
       <Image
-        src={GenshinDiaryLogo.src}
+        src={CURRENT_LOGO}
         width={1280}
         height={720}
         alt='Genshin Logo App'
+        className='size-full object-cover'
       />
       <Badge>Beta</Badge>
     </Link>
