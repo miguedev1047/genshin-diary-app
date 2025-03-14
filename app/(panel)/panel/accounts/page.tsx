20be7fcb5e5ee20b953d29224914df76a1c37859
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-14 17:58:06 -0500

    CHORE: update account page ⚙️

--- app/(panel)/panel/accounts/page.tsx
+++ app/(panel)/panel/accounts/page.tsx
@@ -1,19 +1,19 @@
 import { ContentLayout } from '@/_components/content-layout'
 import { HeaderWrapper } from '@/components/header-wrapper'
 import { getAccounts } from '@/app/(panel)/panel/accounts/_services/fetch'
-import { Card, CardContent } from '@/components/ui/card'
-import { columns } from '@/app/(panel)/panel/accounts/_components/accounts-table/accounts.column'
-import { AccountNoAccess } from '@/app/(panel)/panel/accounts/_components/account-no-access'
-import { currentRole } from '@/data/auth'
-import { User } from '@prisma/client'
+import { Card } from '@/components/ui/card'
+import { accontColumns } from '@/app/(panel)/panel/accounts/_components/accounts-table/accounts.column'
 import { DataTable } from '@/components/data-table'
 import { AccountsHeader } from '@/components/headers/accounts-header'
+import { isCurrentRole } from '@/data/auth'
+import { redirect } from 'next/navigation'
 
-export default async function AccountsPage() {
-  const ACCOUNTS = (await getAccounts()) as Array<User>
+export default async function PanelAccountsPage() {
+  const ACCOUNTS = await getAccounts()
 
-  const ROLE = await currentRole()
-  if (ROLE !== 'OWNER') return <AccountNoAccess />
+  if (await isCurrentRole('USER')) {
+    return redirect('/dashboard')
+  }
 
   return (
     <ContentLayout title='Cuentas'>
@@ -22,12 +22,10 @@ export default async function AccountsPage() {
       </HeaderWrapper>
 
       <Card className='p-6'>
-        <CardContent>
-          <DataTable
-            data={ACCOUNTS ?? []}
-            columns={columns}
-          />
-        </CardContent>
+        <DataTable
+          data={ACCOUNTS ?? []}
+          columns={accontColumns}
+        />
       </Card>
     </ContentLayout>
   )
