b1395a2fd2de1482b7aee730fbb1cfa3631419db
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-14 17:50:32 -0500

    FEAT: add auth helper ⚙️

--- data/auth.ts
+++ data/auth.ts
@@ -9,3 +9,8 @@ export const currentRole = async () => {
   const session = await auth()
   return session?.user?.role
 }
+
+export const isCurrentRole = async (value: string) => {
+  const role = await currentRole()
+  return role === value
+}
