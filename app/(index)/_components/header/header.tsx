e6ba5e0555a7adb4e17cde462235be52d964ca84
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-14 18:01:26 -0500

    CHORE: remove chat ai & add theme toggle button from header component ⚙️

--- app/(index)/_components/header/header.tsx
+++ app/(index)/_components/header/header.tsx
@@ -9,27 +9,30 @@ import { Logo } from '@/app/(index)/_components/logo'
 import { Button } from '@/components/ui/button'
 import { auth } from '@/auth'
 import { User2 } from 'lucide-react'
-import { ChatAI } from '@/app/(panel)/_components/chat-ai'
+import { ThemeToggle } from '@/components/ui/theme-toggle'
 import Link from 'next/link'
 
 export function Header() {
   return (
     <>
-      <header className='sticky top-0 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60 px-16 z-40'>
+      <header className='sticky top-0 w-full bg-background/95 shadow-sm border-b backdrop-blur-sm supports-backdrop-filter:bg-background/60 px-16 z-40'>
         <div className='max-w-7xl mx-auto flex h-16 items-center justify-between'>
-          <nav className='flex flex-grow basis-0'>
+          <nav className='flex grow basis-0'>
             <Logo />
           </nav>
 
           <Navbar />
 
-          <nav className='flex flex-grow basis-0 justify-end'>
-            <PanelButton />
+          <nav className='flex grow basis-0 justify-end'>
+            <div className='flex items-center gap-1'>
+              <PanelButton />
+              <ThemeToggle />
+            </div>
           </nav>
         </div>
       </header>
       <div className='fixed bottom-8 left-8'>
-        <ChatAI />
+        
       </div>
     </>
   )
@@ -46,10 +49,11 @@ async function PanelButton() {
           <Button
             size='icon'
             variant='outline'
+            className='rounded-full w-8 h-8 bg-background'
             asChild
           >
             <Link href='/panel'>
-              <User2 />
+              <User2 className='w-[1.2rem] h-[1.2rem]' />
             </Link>
           </Button>
         </TooltipTrigger>
