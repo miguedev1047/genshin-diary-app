a0fbf01139a0e1f23b28e59a741b28dcc5bad9e1
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-14 23:55:19 -0500

    CHORE: add succes paste icon ⚙️

--- app/(panel)/_components/view-image-input/view-image-input.tsx
+++ app/(panel)/_components/view-image-input/view-image-input.tsx
@@ -6,16 +6,34 @@ import {
 } from '@/components/ui/tooltip'
 import { Input } from '@/components/ui/input'
 import { ViewImageInputProps } from './view-image-input.type'
-import { Clipboard } from 'lucide-react'
+import { Check, Clipboard } from 'lucide-react'
 import { Button } from '@/components/ui/button'
+import { useState } from 'react'
+import { toast } from 'sonner'
+
+const TIME_CLEAR = 1500
+const URL_START = ['https://', 'http://']
 
 export function ViewImageInput(props: ViewImageInputProps) {
   const { disabled, onChange, value, placeholder } = props
+  const [successPaste, setSuccessPaste] = useState(false)
+
+  const VALID_LINK = URL_START.some((star) => value.startsWith(star))
+
+  const onSuccessPaste = (time: number) => {
+    const successPasteTimout = setTimeout(() => {
+      setSuccessPaste(false)
+    }, time)
+    return () => clearTimeout(successPasteTimout)
+  }
 
   const handlePaste = async () => {
+    if (VALID_LINK) return toast.error('Link inválido')
+
     try {
       const text = await navigator.clipboard.readText()
       onChange(text)
+      onSuccessPaste(TIME_CLEAR)
     } catch (err) {
       console.error('Failed to read clipboard contents: ', err)
     }
@@ -28,7 +46,7 @@ export function ViewImageInput(props: ViewImageInputProps) {
         placeholder={placeholder}
         disabled={disabled}
         onChange={(e) => onChange(e.target.value)}
-        className='peer pe-9'
+        className='peer pe-10'
       />
       <TooltipProvider>
         <Tooltip>
@@ -41,7 +59,11 @@ export function ViewImageInput(props: ViewImageInputProps) {
               size='icon'
               className='absolute right-0 top-0 mx-1 hover:bg-transparent'
             >
-              <Clipboard className='h-4 w-4' />
+              {successPaste ? (
+                <Check className='h-4 w-4 text-lime-400' />
+              ) : (
+                <Clipboard className='h-4 w-4' />
+              )}
               <span className='sr-only'>Pegar</span>
             </Button>
           </TooltipTrigger>
