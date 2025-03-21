4ce9f068330f4f029a201233c1dc3fcb11e017d4
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-21 18:30:50 -0500

    REFACTOR: simplify home page by removing character fetching and components

---
+++ app/(index)/(routes)/(home)/_components/home-hero/home-hero.tsx
@@ -0,0 +1,106 @@
+'use client'
+
+import { DesktopThumbnail } from '@/assets/images/_index'
+import { FocalLight } from '@/components/focal-light'
+import { BorderBeam } from '@/components/magicui/border-beam'
+import { AnimatedShinyText } from '@/components/ui/animated-shiny-text'
+import { RainbowButton } from '@/components/ui/rainbow-button'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function HomeHero() {
+  return (
+    <div className='relative mx-auto my-10 flex max-w-7xl flex-col items-center justify-center'>
+      <div className='px-4 py-10 md:py-20'>
+        <AnimatedShinyText className='relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300'>
+          {'Tu web amigable para guías de Genshin Impact'
+            .split(' ')
+            .map((word, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 0, filter: 'blur(4px)', y: 10 }}
+                animate={{ opacity: 1, filter: 'blur(0px)', y: 0 }}
+                transition={{
+                  duration: 0.3,
+                  delay: index * 0.1,
+                  ease: 'easeInOut',
+                }}
+                className='mr-2 inline-block'
+              >
+                {word}
+              </motion.span>
+            ))}
+        </AnimatedShinyText>
+        <motion.p
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 0.8,
+          }}
+          className='relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400'
+        >
+          Esta plataforma ofrece guías simples y accesibles sobre los personajes
+          de Genshin Impact.
+        </motion.p>
+        <motion.div
+          initial={{
+            opacity: 0,
+          }}
+          animate={{
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.3,
+            delay: 1,
+          }}
+          className='relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4'
+        >
+          <Link
+            href='/characters'
+            className='animate-fade-in -translate-y-4 opacity-0 [--animation-delay:600ms]'
+          >
+            <RainbowButton>Ver personajes</RainbowButton>
+          </Link>
+        </motion.div>
+
+        <div className='relative'>
+          <FocalLight />
+
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 10,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 1.2,
+            }}
+            className='relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900'
+          >
+            <BorderBeam />
+
+            <div className='w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700'>
+              <Image
+                src={DesktopThumbnail.src}
+                alt='Landing page preview'
+                className='aspect-[16/9] h-auto w-full object-cover'
+                height={1000}
+                width={1000}
+              />
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- app/(index)/(routes)/(home)/page.tsx
+++ app/(index)/(routes)/(home)/page.tsx
@@ -1,17 +1,10 @@
 import { ContentLayout } from '@/app/(index)/_components/content-layout'
-import { PageProps } from '@/app/(index)/(routes)/(home)/_types'
-import { getCharacters } from '@/app/(index)/(routes)/(home)/_services/fetch'
-import { HomeHero } from '@/app/(index)/(routes)/(home)/_sections/home-hero'
-import { HomeCharacters } from '@/app/(index)/(routes)/(home)/_sections/home-characters'
+import { HomeHero } from '@/app/(index)/(routes)/(home)/_components/home-hero'
 
-export default async function Home(props: PageProps) {
-  const { searchParams: PARAMS } = props
-  const CHARACTERS = await getCharacters(PARAMS)
-
+export default function Home() {
   return (
     <ContentLayout particles>
       <HomeHero />
-      <HomeCharacters data={CHARACTERS} />
     </ContentLayout>
   )
 }
