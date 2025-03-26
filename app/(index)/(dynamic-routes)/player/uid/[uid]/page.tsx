e5aeec544e905f4625136c3b3351ad26ad5ce99f
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-26 14:26:08 -0500

    FEAT: setup player page ✅

---
+++ app/(index)/(dynamic-routes)/player/uid/[uid]/page.tsx
@@ -0,0 +1,9 @@
+import { PlayerCard } from '@/app/(index)/(dynamic-routes)/player/uid/[uid]/_components/player-card'
+
+export default async function PlayerUidPage() {
+  return (
+    <section className='w-full'>
+      <PlayerCard />
+    </section>
+  )
+}
