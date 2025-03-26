b3802dff3aa92272f2dd241c9082ab0e0ef49e0d
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-26 14:26:40 -0500

    FEAT: add player tabs component 🎨

---
+++ app/(index)/(dynamic-routes)/player/uid/[uid]/_components/player-tabs/player-tabs.type.ts
@@ -0,0 +1,10 @@
+import { Character } from '@/types/player-data.type'
+
+export type CharacterLayoutProps = {
+  character: Character
+  children: React.ReactNode
+}
+
+export type CharacterCardProps = {
+  character: Character
+}
