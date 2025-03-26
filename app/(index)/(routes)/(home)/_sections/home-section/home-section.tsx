226434943d481be0eb35da97127df591f2bb34d3
miguel angel <miguelangeljimenez1047@gmail.com>
2025-03-26 14:23:32 -0500

    FEAT: add home section ⚙️

---
+++ app/(index)/(routes)/(home)/_sections/home-section/home-section.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { Logo } from '@/app/(index)/_components/logo'
+import { HomeHero } from '@/app/(index)/(routes)/(home)/_components/home-hero'
+
+export function HomeSection() {
+  return (
+    <section className='relative mx-auto my-10 max-w-7xl grid place-items-center space-y-6 pb-16'>
+      <article className='block lg:hidden'>
+        <Logo className='w-full max-w-[320px]' />
+      </article>
+      
+      <HomeHero />
+    </section>
+  )
+}
